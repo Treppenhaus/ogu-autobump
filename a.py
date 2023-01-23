cd8a68acd9908025a99673589da7335c4c1c970c
treppi✨ <53356121+Treppenhaus@users.noreply.github.com>
2023-01-23 20:38:29 +0100

    Update a.py

--- a.py
+++ a.py
@@ -7,13 +7,18 @@ import undetected_chromedriver as uc
 # VARIABLES TO CHANGE
 username = "" #ur username
 password = "" #ur password
-threadurl = "" #ur threadurl
-message = "" # message to send
-bump_delay = 60 * 31 # 31 minutes
 
 
-driver = uc.Chrome(use_subprocess=True)
-wait = WebDriverWait(driver, 20)
+# make sure there is SAME amount of messages and threads! 4 threads = 4 messages in array!
+threads = ["", "", "", ""]
+messages = ["", "", "", ""]
+
+
+bump_delay = (60 * 30 / len(threads)) + 60
+print(f"bump delay: {bump_delay}")
+
+driver = None
+wait = None
 
 def c(selector):
     wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, selector))).click()
@@ -22,6 +27,7 @@ def e(selector, inp):
     wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, selector))).send_keys(inp)
 
 def login():
+    
     driver.get("https://ogu.gg/login")
     time.sleep(6)
     e("#fullcontainment > div > form:nth-child(3) > table > tbody > tr.tr-rounded > td > label > input", username)
@@ -29,17 +35,39 @@ def login():
     time.sleep(1)
     c("#fullcontainment > div > form:nth-child(3) > table > tbody > tr:nth-child(4) > td > span > input")
 
-def bump():
+def bump(threadurl, message):
+    login()
+    time.sleep(10)
     driver.get(threadurl)
     time.sleep(6)
     e("#message", message)
+    time.sleep(1)
     c("#quick_reply_submit")
+    time.sleep(1)
     print("bump!")
+    driver.quit()
 
-login()
-time.sleep(10)
 while(1):
-    bump()
-    time.sleep(bump_delay)
+    
+    a = 0
+    while(a < len(threads)):
+        try:
+            turl = threads[a]
+            msg = messages[a]
+        
+            driver = uc.Chrome(use_subprocess=True)
+            wait = WebDriverWait(driver, 20)
+            bump(turl, msg)
+            
+            i = 0
+            a += 1
+            while(i < bump_delay):
+                i += 1
+                time.sleep(1)
+                print(f"bumping again in {bump_delay-i} seconds")
+        except:
+            print("something went wrong! trying again in 60 seconds!")
+            time.sleep(60)
+            
 
 # Close the webdriver
